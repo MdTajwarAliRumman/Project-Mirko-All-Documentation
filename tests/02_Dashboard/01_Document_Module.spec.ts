53a0adbd1ac73a7af761fb15f4ec74c6ca7ea148
MdTajwarAliRumman <tajwaralirumman@gmail.com>
2026-04-09 14:48:04 +0600

    Dashboard Settings

---
+++ tests/02_Dashboard/01_Document_Module.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from '@playwright/test';
+import { HomePage } from '../../src/pages/HomePage';
+import { AuthPage } from '../../src/pages/AuthPage';
+
+
+test.describe('Authentication Flow', () => {
+    let authPage: AuthPage;
+    let homePage: HomePage;
+
+    test.beforeEach(async ({ page }) => {
+        authPage = new AuthPage(page);
+        homePage = new HomePage(page);
+
+
+        await homePage.goToURL();
+
+        await homePage.userIcon.click();
+    });
+
+    test('Verify "Login section" is visible', async ({ page }) => {
+        await expect(page.getByText('Welcome Back.')).toBeVisible();
+    });
+
+    test('Verify "User login" is Successful', async ({ page }) => {
+        await authPage.emailInput.fill(process.env.EMAIL!);
+        await authPage.passwordInput.fill(process.env.USER_PASSWORD!);
+        await authPage.loginButton.click();
+        await expect(page.getByText('Login Successful')).toBeVisible();
+    });
+
+    test('Verify Dashboard is visible', async ({ page }) => {
+        await expect(page.getByText('Dashboard')).toBeVisible();
+    });
+
+});
